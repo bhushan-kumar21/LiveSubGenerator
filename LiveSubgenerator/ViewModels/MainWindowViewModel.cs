1f8da2ee117cd9647b5ca2cdd3494deb8cc25a8c
bhushan-kumar21 <72263232+bhushan-kumar21@users.noreply.github.com>
2026-01-11 19:45:26 -0600

    initial commit

---
+++ LiveSubgenerator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LiveSubgenerator.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
